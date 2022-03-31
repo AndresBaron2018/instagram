1329de26b0a1bc55c767bbccbe6fe9c6cceffa80
andresbaro2018 <andresbaron2018@outlook.com>
2022-03-30 23:54:51 -0500

    The project begins

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import * as ReactDOMClient from 'react-dom/client';
+
+
+const container = document.getElementById('app');
+
+const root = ReactDOMClient.createRoot(container);
+
+root.render(<h1>'You are rich #1'</h1>);
